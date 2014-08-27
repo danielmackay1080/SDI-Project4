2da70dd2efeee967a4a9d15781aa06bf9affc8b7
Daniel Mackay <danielmackay@daniels-mbp.flowtrinidad.com>
2014-08-27 12:41:38 -0400

    The second commit

--- mackay_daniel_project4.js
+++ mackay_daniel_project4.js
@@ -1,1 +1,20 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+// Daniel Mackay
+// SDI 1408
+// Project 4
+
+// Sting Problem 1
+// Does a string follow a 123-456-7890 pattern like a phone number?
+
+var stringPhoneNum = '123-456-7890';
+var phonePattern = true;
+
+var stringFunction = function (string, phonePattern) {
+	var mySubstring = stringPhoneNum.substring(0,11)
+		if (stringPhoneNum === 11){
+		console.log('The answer is true');
+			}else{
+			console.log('The answer is false.');
+			}
+};
+stringFunction(stringPhoneNum, phonePattern);
